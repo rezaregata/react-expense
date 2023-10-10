ba152d69e39c6d18a06cfdc1bb98265196927d45
rezaregata <52247983+rezaregata@users.noreply.github.com>
2023-10-10 09:21:20 +0700

    add expense filter

---
+++ src/components/Expenses/ExpenseFilter.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import "./ExpenseFilter.css";
+
+export default function ExpensesFilter(props) {
+  const changeYearHandler = (event) => {
+    console.log(event.target.value);
+    props.expenseYear(event.target.value);
+  };
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by year</label>
+        <select onChange={changeYearHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+}
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,22 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 export default function Expenses(props) {
+  const [expenseYear, setExpenseYear] = useState(undefined);
+
+  const onChangeExpenseYear = (year) => {
+    setExpenseYear(year);
+    // if (expenseYear === '2019') {
+    console.log(year);
+    // }
+  };
   return (
     <Card className="expenses">
+      <ExpensesFilter expenseYear={onChangeExpenseYear} />
       <ExpenseItem
         title={props.expenses[0].title}
         amount={props.expenses[0].amount}
